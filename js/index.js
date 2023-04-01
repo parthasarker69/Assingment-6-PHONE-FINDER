8f7a4d68defc57eb35dd425c6b838bbaa0828f13
parthasarker69 <parthasarkery9@gmail.com>
2023-04-02 00:17:17 +0600

    data loading done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,22 @@
+
+// Taking value from searchBox and loading data from the server 
+
 const loadDataAsSearched = () => {
     const searchedText = document.getElementById('input-value').value;
-    console.log(searchedText)
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchedText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showPhonesOnDisplay(data.data))
+}
+
+// Showing the searched Phone in the screen 
+
+const showPhonesOnDisplay = phones => {
+
+
+    phones.forEach(phone => {
+        console.log(phone)
+
+    });
+    console.log(phones)
 }
